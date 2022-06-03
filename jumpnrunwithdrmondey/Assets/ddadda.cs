083ddbd49d701813c83a363e4efb935aa381602a
Moonrise45555 <106400298+Moonrise45555@users.noreply.github.com>
2022-06-03 17:38:41 +0200

    NOO WHY
    dsad

---
+++ jumpnrunwithdrmondey/Assets/ddadda.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ddadda : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // da do the do the doooooooooo lo l
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
